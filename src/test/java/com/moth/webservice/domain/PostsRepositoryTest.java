5aa7e1fa8b572aa60780a9c2a363ab77edb2e029
kdpark <kdpark1623@gmail.com>
2020-07-04 16:21:46 +0900

    basetimeentity test code 추가

--- src/test/java/com/moth/webservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/moth/webservice/domain/PostsRepositoryTest.java
@@ -2,7 +2,9 @@ package com.moth.webservice.domain;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.After;
@@ -34,5 +36,18 @@ public class PostsRepositoryTest {
 		
 		assertThat(posts.getTitle(), is("test title"));
         assertThat(posts.getContent(), is("test contents"));
+	}
+	
+	@Test
+	public void BaseTimeEntity_등록() {
+		LocalDateTime now = LocalDateTime.now();
+		postsRepository.save(Posts.builder().title("test title").author("moth@gmail.com").content("test content").build());
+		
+		List <Posts> postsList = postsRepository.findAll();
+		
+		Posts posts = postsList.get(0);
+		
+		assertTrue(posts.getCreatedDate().isAfter(now));
+		assertTrue(posts.getModifiedDate().isAfter(now));
 	}
 }
