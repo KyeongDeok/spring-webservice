7650722e253b587b60e6a896b97f54111bce7f75
kdpark <kdpark1623@gmail.com>
2020-07-07 16:30:10 +0900

    member login service repo entity 구현

---
+++ src/main/java/com/moth/webservice/domain/MemberDto.java
@@ -0,0 +1,34 @@
+package com.moth.webservice.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class MemberDto {
+	private Long id;
+	private String email;
+	private String password;
+	private String createdDate;
+	private String modifiedDate;
+	
+	public MemberEntity toEntity() {
+		return MemberEntity.builder()
+				.id(id)
+				.email(email)
+				.password(password)
+				.build();
+	}
+	
+	@Builder
+	public MemberDto(Long id, String email, String password) {
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+}
--- src/main/java/com/moth/webservice/service/MemberService.java
+++ src/main/java/com/moth/webservice/service/MemberService.java
@@ -1,15 +1,52 @@
 package com.moth.webservice.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.moth.webservice.config.Role;
+import com.moth.webservice.domain.MemberDto;
+import com.moth.webservice.domain.MemberEntity;
+import com.moth.webservice.domain.MemberRepository;
 
 public class MemberService implements UserDetailsService {
-
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private MemberRepository memberRepository;
+	
+	@Transactional
+	public Long joinUser(MemberDto dto) {
+		
+		//비밀번호 암호화
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+		
+		return memberRepository.save(dto.toEntity()).getId();
 	}
-
+	
+	@Override
+	public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+		
+		Optional <MemberEntity> userEntityWrapper = memberRepository.findByEmail(userEmail);
+		MemberEntity memberEntity = userEntityWrapper.get();
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		if(("admin@example.com").equals(userEmail)) {
+			authorities.add(new SimpleGrantedAuthority(Role.ADMIN.getValue()));
+		}else {
+			authorities.add(new SimpleGrantedAuthority(Role.MEMBER.getValue()));
+		}
+		
+		return new User(memberEntity.getEmail(), memberEntity.getPassword(), authorities);
+	}
 }
