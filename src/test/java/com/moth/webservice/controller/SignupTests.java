6db192f42599407bc7fc2823034e56a4e931d726
kdpark <kdpark1623@gmail.com>
2020-07-12 09:55:28 +0900

    thyme leaf 권한 에따라 버튼 노출

--- src/test/java/com/moth/webservice/controller/SignupTests.java
+++ src/test/java/com/moth/webservice/controller/SignupTests.java
@@ -1,17 +1,16 @@
 package com.moth.webservice.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,8 +33,9 @@ public class SignupTests {
 	public void 회원가입_csrf토큰을_헤더에_담아전송후_200을_응답받는다 () throws Exception {
 		
 		mvc.perform(post("/user/signup")
+		        .contentType(MediaType.APPLICATION_JSON)
 				.param("email", "test@test.com")
-				.param("password", new BCryptPasswordEncoder().encode("123"))
+				.param("password", "123")
 				.with(csrf()))
 		.andDo(print())
 		.andExpect(status().is2xxSuccessful());
